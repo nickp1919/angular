eb27ee325b8679135e504b6979f8869025ebe090
nickp1919 <64270163+nickp1919@users.noreply.github.com>
2020-08-11 18:58:52 +0300

    Refactoring HTML, correct atribute class

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -11,15 +11,15 @@ import {TableService} from "./table.service";
 
 export class TableComponent implements OnInit {
 
-  namecheckbox;
+  nameCheckbox: string;
   headers: Array<string> = [];
   subheaders: Array<string> = [];
-  keyfordata: Array<string> = [];
+  keysForData: Array<string> = [];
   data: Array<object>;
   colspan: object = {};
-  colspandef: object = {};
+  colspanDefault: object = {};
   display: object = {};
-  setchecked: object = {};
+  setChecked: object = {};
   btn = ['btn btn-secondary active', 'btn btn-secondary']
 
 
@@ -29,23 +29,23 @@ export class TableComponent implements OnInit {
 
   ngOnInit() {
     this.jsonService.getJson().subscribe(getdata => {
-      this.namecheckbox = getdata.head;
+      this.nameCheckbox = getdata.head;
       this.data = getdata.data;
       getdata.head.forEach( x => {
          this.headers.push(x.name);
          this.display[x.name] = false;
-         this.setchecked[x.name] = true;
+         this.setChecked[x.name] = true;
          if (x['sub-head']) {
               this.colspan[x.name] = x['sub-head'].length;
-              this.colspandef[x.name] = x['sub-head'].length;
+              this.colspanDefault[x.name] = x['sub-head'].length;
               x['sub-head'].forEach( v => {
               this.subheaders.push(v.name);
-              this.keyfordata.push(v.name);
+              this.keysForData.push(v.name);
               this.display[v.name] = false;
-              this.setchecked[v.name] = true;
+              this.setChecked[v.name] = true;
               });
           } else {
-              this.keyfordata.push(x.name);
+              this.keysForData.push(x.name);
             }
       });
     });
@@ -59,45 +59,45 @@ export class TableComponent implements OnInit {
       }
   }
 
-  displayOffHeadandSub(event, namehead: any, listsub: any) {
+  displayOffHeadAndSub(event, nameHead: any, listSub: any) {
     if (!event.target.checked) {
-      this.setchecked[namehead] = false;
-      this.display[namehead] = true;
-      listsub.forEach( s => {
+      this.setChecked[nameHead] = false;
+      this.display[nameHead] = true;
+      listSub.forEach( s => {
         this.display[s.name] = true;
-        this.colspan[namehead] = 0;
-        this.setchecked[s.name] = false;
+        this.colspan[nameHead] = 0;
+        this.setChecked[s.name] = false;
       });
     } else {
-        this.display[namehead] = false;
-        this.setchecked[namehead] = true;
-        listsub.forEach( s => {
+        this.display[nameHead] = false;
+        this.setChecked[nameHead] = true;
+        listSub.forEach( s => {
           this.display[s.name] = false;
-          this.setchecked[s.name] = true;
-          this.colspan[namehead] = listsub.length;
+          this.setChecked[s.name] = true;
+          this.colspan[nameHead] = listSub.length;
         });
       }
   }
 
-  displayOffSub(event, namesub: any, namehead: any) {
+  displayOffSub(event, nameSub: any, nameHead: any) {
     if (!event.target.checked) {
-      this.display[namesub] = true;
-      if (this.colspan[namehead] === 1) {
-        this.display[namehead] = true;
+      this.display[nameSub] = true;
+      if (this.colspan[nameHead] === 1) {
+        this.display[nameHead] = true;
       }
-      this.colspan[namehead] --;
-      this.setchecked[namehead] = false;
-      this.setchecked[namesub] = false;
+      this.colspan[nameHead] --;
+      this.setChecked[nameHead] = false;
+      this.setChecked[nameSub] = false;
     } else {
-        this.setchecked[namesub] = true;
-        this.display[namesub] = false;
-        if (this.colspan[namehead] === 0) {
-          this.display[namehead] = false;
+        this.setChecked[nameSub] = true;
+        this.display[nameSub] = false;
+        if (this.colspan[nameHead] === 0) {
+          this.display[nameHead] = false;
         }
-        if (this.colspan[namehead] === this.colspandef[namehead] - 1) {
-          this.setchecked[namehead] = true;
+        if (this.colspan[nameHead] === this.colspanDefault[nameHead] - 1) {
+          this.setChecked[nameHead] = true;
         }
-        this.colspan[namehead] ++;
+        this.colspan[nameHead] ++;
       }
   }
 
